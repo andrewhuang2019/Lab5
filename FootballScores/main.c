7e1b0f865b0131073b64b9b9e073a7133632ecf7
Andysword <andrewhuang2019@gmail.com>
2024-03-09 21:39:57 -0600

    Fixed bugs
    Fixed bugs and removed unnecessary code (extra variables)

--- FootballScores/main.c
+++ FootballScores/main.c
@@ -4,24 +4,25 @@ void scoringCombinations(int score);
 
 int main(){
 
-    while(1 == 1){
+    while(1){
 
-        printf("Enter 0 or 1 to STOP");
+        printf("Enter 0 or 1 to STOP\n");
 
         printf("Enter the NFL Score: ");
+
+        fflush(stdout);
 
         int score;
 
         scanf("%d", &score);
 
         if (score <= 1){
-            goto finish;
+            break;
         }
 
         scoringCombinations(score);
     }
 
-    finish:;
     return 0;
 }
 
@@ -29,22 +30,17 @@ void scoringCombinations(int score){
     if (score <= 2){
         printf("Invalid score");
     } else {
-        for (int numTouchDown = 0; numTouchDown < score / 6; numTouchDown++){
-            int td = 0;
-            int td2 = 0;
-            int tdfg = 0;
-            int fg = 0;
-            int safety = 0;
+        for (int numTouchDownPlusTwo = 0; numTouchDownPlusTwo < score / 8; numTouchDownPlusTwo++){
 
             for (int numTouchDownPlusField = 0; numTouchDownPlusField < score / 7; numTouchDownPlusField++){
 
-                for (int numTouchDownPlusTwo = 0; numTouchDownPlusTwo < score / 8; numTouchDownPlusTwo++){
+                for (int numTouchDown = 0; numTouchDown < score / 6; numTouchDown++){
                     for (int numFieldGoal = 0; numFieldGoal < score / 3; numFieldGoal++){
-                        for (int numSafety; numSafety < score / 2; numSafety++){
+                        for (int numSafety = 0; numSafety < score / 2; numSafety++){
                             
                             if ((numTouchDown * 6) + (numTouchDownPlusField * 7) + (numTouchDownPlusTwo * 8) + 
                                 (numFieldGoal * 3) + (numSafety * 2) == score){
-                                    printf("%d TD + 2pt,%d TD + FG,%d TD,%d 3pt FG,%d Safety", 
+                                    printf("%d TD + 2pt,%d TD + FG,%d TD,%d 3pt FG,%d Safety\n", 
                                     numTouchDownPlusTwo, numTouchDownPlusField, numTouchDown, numFieldGoal, numSafety);
                             }
 
